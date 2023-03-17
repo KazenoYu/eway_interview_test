a72e7dce7aeb9a72d313a9e154e0b9773323e4cf
Eating <f.t.aisamc@gmail.com>
2023-03-17 12:19:52 +0800

    update: vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,5 @@ module.exports = defineConfig({
 });
 
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/eway_interview_test/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/ewill_test/" : "/",
 };
