170b9851fcdc80eb416a5b62425245478a3ae955
Eating <f.t.aisamc@gmail.com>
2023-03-17 12:11:51 +0800

    fix: module.exports Product Name

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,6 @@ module.exports = defineConfig({
 });
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/eric-project/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/eway_interview_test/" : "/",
 };
